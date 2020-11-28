f00cda8843a502358da4ef747a61bb0c90cabd6f
Gustavo Sousa <gussousa@gmail.com>
2020-11-28 02:46:46 -0300

    App is fully functional, only the search is not implemented yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Generating flats'
+types = %w[apartment bedroom livingroom]
+8.times do
+  flat = Flat.create!(
+    name: Faker::Games::HalfLife.location,
+    address: Faker::Address.full_address,
+    description: Faker::Movies::Ghostbusters.quote,
+    price_per_night: (45..650).to_a[rand(651 - 45)],
+    number_of_guests: (1..5).to_a[rand(6)],
+    image_url: "https://source.unsplash.com/1600x900/?#{types[rand(3)]},#{rand(11)}"
+  )
+  puts "#{flat.name} created"
+end
